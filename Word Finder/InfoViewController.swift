cdcb879b41bfe447c2d2b617ff4f1505d4b42130
markbrody <mark.brody@gmail.com>
2016-03-05 12:10:22 -0600

    add toolbar and transition to info/copyright screen

---
+++ Word Finder/InfoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InfoViewController.swift
+//  Word Finder
+//
+//  Created by Mark Brody on 3/5/16.
+//  Copyright © 2016 Mark Brody. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+
+}
